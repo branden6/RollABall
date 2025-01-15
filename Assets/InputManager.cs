88f53eab541a3ed2356a539b912c4d7b97a5f1f7
branden kennedy <brandenk88@gmail.com>
2025-01-15 08:09:35 -0800

    Added back in Debug.Log code
    added in the Debug.Log code pieces again in order to have the same output as the uploaded demo video.

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -14,20 +14,25 @@ public class InputManager : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W)){
             inputVector += Vector2.up;  //if w is pressed, go up
+            Debug.Log("Up");
        }
        if (Input.GetKey(KeyCode.S)){
             inputVector += Vector2.down;    //if S is pressed, go down
+            Debug.Log("Down");
        }
        if (Input.GetKey(KeyCode.A)){
             inputVector += Vector2.left;    //if A is pressed, go left
+            Debug.Log("Left");
        }
        if (Input.GetKey(KeyCode.D)){
             inputVector += Vector2.right;   //if D is pressed, go right
+            Debug.Log("Right");
        }
 
         OnMove?.Invoke(inputVector);
 
         if (Input.GetKeyDown(KeyCode.Space)){
+          Debug.Log("Spacebar");
           OnJump?.Invoke();
         }
     }
